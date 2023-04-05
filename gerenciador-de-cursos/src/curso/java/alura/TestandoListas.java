e86808946e083822b278f3919bdf2366674a7ac7
wagner <wagner.santos@accurate.com.br>
2023-04-05 14:15:19 -0300

    Testando listas

---
+++ gerenciador-de-cursos/src/curso/java/alura/TestandoListas.java
@@ -0,0 +1,38 @@
+package curso.java.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas {
+    public static void main(String[] args) {
+        String aula1 = "Modelando a classe Aula";
+        String aula2 = "Conhecendo mais de listas";
+        String aula3 = "Trabalhando com Cursos e Sets";
+
+        ArrayList<String> aulas = new ArrayList<>();
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+        System.out.println(aulas);
+
+        aulas.remove(0);
+        System.out.println(aulas);
+
+        for (String aula : aulas) {
+            System.out.println("Aula: " + aula);
+        }
+
+        String primeiraAula = aulas.get(0);
+        System.out.println("A primeira aula é " + primeiraAula);
+
+        aulas.forEach(aula -> {
+            System.out.println("Percorrendo:");
+            System.out.println("Aula " + aula);
+        });
+
+        Collections.sort(aulas);
+        System.out.println("Depois de ordenado:");
+        System.out.println(aulas);
+    }
+}
