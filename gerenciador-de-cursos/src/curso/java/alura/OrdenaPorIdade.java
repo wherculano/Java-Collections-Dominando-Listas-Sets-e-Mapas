4cc0a3a14d58bf7e69b180ecceb7747b6d42e9d9
wagner <wagner.santos@accurate.com.br>
2023-04-05 22:32:49 -0300

    Exemplo de como utilizar o TreeSet

---
+++ gerenciador-de-cursos/src/curso/java/alura/OrdenaPorIdade.java
@@ -0,0 +1,10 @@
+package curso.java.alura;
+
+import java.util.Comparator;
+
+public class OrdenaPorIdade implements Comparator<Funcionario> {
+    @Override
+    public int compare(Funcionario funcionario, Funcionario outroFuncionario) {
+        return funcionario.getIdade() - outroFuncionario.getIdade();
+    }
+}
