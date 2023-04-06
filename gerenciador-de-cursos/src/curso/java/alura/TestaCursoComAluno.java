500b38fca029f82333def339e06767ac61dfd198
wagner <wagner.santos@accurate.com.br>
2023-04-05 22:08:26 -0300

    Testando metodos hashCode e equals

--- gerenciador-de-cursos/src/curso/java/alura/TestaCursoComAluno.java
+++ gerenciador-de-cursos/src/curso/java/alura/TestaCursoComAluno.java
@@ -20,8 +20,18 @@ public class TestaCursoComAluno {
         javaColecoes.matricula(a3);
 
         System.out.println("Todos os alunos matriculados: ");
-        javaColecoes.getAlunos().forEach(aluno -> {
-            System.out.println(aluno);
-        });
+        javaColecoes.getAlunos().forEach(System.out::println);
+
+        System.out.println("O Aluno "+ a1 +" está matriculado?");
+        System.out.println(javaColecoes.estaMatriculado(a1));
+
+        Aluno turini = new Aluno("Rodrigo Turini", 34672);
+        System.out.println("E esse Turini, está matriculado?");
+        System.out.println(javaColecoes.estaMatriculado(turini));
+
+        System.out.println("O a1 é equals ao Turini?");
+        System.out.println(a1.equals(turini));
+
+
     }
 }
