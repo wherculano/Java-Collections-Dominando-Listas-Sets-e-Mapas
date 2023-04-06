b7b604bacf753ba1bd7db45ae8d2a6c6de0141db
wagner <wagner.santos@accurate.com.br>
2023-04-05 22:08:06 -0300

    Adicionados metodos hasCode e equals

--- gerenciador-de-cursos/src/curso/java/alura/Aluno.java
+++ gerenciador-de-cursos/src/curso/java/alura/Aluno.java
@@ -6,6 +6,9 @@ public class Aluno {
     private int numeroMatricula;
 
     public Aluno(String nome, int numeroMatricula) {
+        if (nome == null) {
+            throw new NullPointerException("Nome não pode ser nulo");
+        }
         this.nome = nome;
         this.numeroMatricula = numeroMatricula;
     }
@@ -21,5 +24,16 @@ public class Aluno {
     @Override
     public String toString() {
         return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Aluno outroAluno = (Aluno) obj;
+        return this.nome.equals(outroAluno.nome);
+    }
+
+    @Override
+    public int hashCode(){
+        return this.nome.hashCode();
     }
 }
